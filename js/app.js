3095d74a78977276c4b443a1f29d934f7266ec22
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-02-28 22:00:23 +0600

    display phone

--- js/app.js
+++ js/app.js
@@ -11,35 +11,39 @@ const phoneSearch = _ => {
         .then(phones => {
             console.log(phones.data);
 
-            document.getElementById('display-total').innerText = `${phones.data.length} Phones Found`
-
-            // phones.forEach(phone => {
-            //     console.log(phone);
-            // });
+            document.getElementById('display-total').innerText = `${phones.data.length} Phones Found`;
 
             const displayPhones = document.getElementById('display-phones');
-
             const div = document.createElement('div');
 
-            div.innerHTML = `
-            <div  class="col">
-                <div class="card h-100">
-                <img src="" class="card-img-top" alt="Phone image" />
-                <div class="card-body">
-                    <h3 class="card-title">Phone Name</h3>
-                    <h5 class="card-title text-muted">Phone Brand</h5>
-                    <p class="card-text">
-                    Phone This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                    </p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-                </div>
-            </div>
-            `
+            phones.data.forEach(phone => {
+                console.log(phone);
+                console.log(phone.brand);
+                console.log(phone.phone_name);
+                console.log(phone.image);
+                console.log(phone.slug);
+
+                div.innerHTML = `
+                    <div  class="col">
+                        <div class="card h-100">
+                        <img src="${phone.image}" class="image-fluid card-img-top p-3" alt="${phone.phone_name} image" />
+                        <div class="card-body">
+                            <h3 class="card-title">${phone.phone_name}</h3>
+                            <h5 class="card-title text-muted">${phone.brand}</h5>
+                            <p class="card-text">
+                            Phone This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                            </p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">${phone.slug}</small>
+                        </div>
+                        </div>
+                    </div>
+                    `
+                displayPhones.appendChild(div);
+            });
         })
     } catch (error) {
         console.log('Something Went Wrong!');
