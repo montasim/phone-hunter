7f4b6dba8bf33fcf4e7fa94dbd8e29a325601716
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-01 09:35:16 +0600

    empty search

--- js/app.js
+++ js/app.js
@@ -2,111 +2,97 @@
 const phoneSearch = _ => {
     // display spinner 
     document.getElementById('spinner').style.display = 'block';
+
     // get search input
     const searchText = document.getElementById('search-input').value;
 
-    try {
-        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    if(searchText == ''){
+        // hide spinner 
+        document.getElementById('spinner').style.display = 'none';
 
-        fetch(url)
-        .then(response => response.json())
-        .then(phones => {
-            // get display field id
-            const displayPhones = document.getElementById('display-phones');
-
-            console.log(phones);
-            console.log(phones.status);
-            
-            if(phones.status == true){
-                // clear previous result
-                displayPhones.innerHTML = '';
-
-                // display total result
-                document.getElementById('display-total').innerHTML = `
-                <div class="alert alert-success" role="alert">
-                    ${phones.data.length} Phones Found
-                </div>`;
-
-                phones.data.forEach(phone => {
-                    console.log(phone);
-                    console.log(phone.brand);
-                    console.log(phone.phone_name);
-                    console.log(phone.image);
-                    console.log(phone.slug);
-
-                    // create a div
-                    const div = document.createElement('div');
-
-                    // display each result
-                    div.innerHTML = `
-                        <div  class="col">
-                            <div class="card h-100">
-                            <img src="${phone.image}" class="image-fluid card-img-top p-3" alt="${phone.phone_name} image" />
-                            <div class="card-body">
-                                <h3 class="card-title">${phone.phone_name}</h3>
-                                <h5 class="card-title text-muted">${phone.brand}</h5>
-                                <p class="card-text">
-                                Phone This is a wider card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit
-                                longer.
-                                </p>
+        // display no data
+        document.getElementById('display-total').innerHTML = `
+        <div class="alert alert-danger" role="alert">
+            Search Field is Empty!
+        </div>
+        `
+    }
+    else{
+        try {
+            const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    
+            fetch(url)
+            .then(response => response.json())
+            .then(phones => {
+                // get display field id
+                const displayPhones = document.getElementById('display-phones');
+                
+                if(phones.status == true){
+                    // clear previous result
+                    displayPhones.innerHTML = '';
+    
+                    // display total result
+                    document.getElementById('display-total').innerHTML = `
+                    <div class="alert alert-success" role="alert">
+                        ${phones.data.length} Phones Found
+                    </div>`;
+    
+                    console.log(phones);
+    
+    
+                    phones.data.forEach(phone => {
+                        // create a div
+                        const div = document.createElement('div');
+    
+                        // display each result
+                        div.innerHTML = `
+                            <div  class="col">
+                                <div class="card h-100">
+                                <img src="${phone.image}" class="image-fluid card-img-top p-3" alt="${phone.phone_name} image" />
+                                <div class="card-body">
+                                    <h3 class="card-title">${phone.phone_name}</h3>
+                                    <h5 class="card-title text-muted">${phone.brand}</h5>
+                                    <p class="card-text">
+                                    Phone This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit
+                                    longer.
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="text-primary" 
+                                    onclick="displayPhoneDetails(${phone.slug})">See Details</div>
+                                </div>
+                                </div>
                             </div>
-                            <div class="card-footer">
-                                <small class="text-muted">${phone.slug}</small>
-                                <br>
-                                <a class="" 
-                                onclick="displayDetails(${phone.slug})">See Details</a>
-                            </div>
-                            </div>
-                        </div>
-                        `
-                    displayPhones.appendChild(div);
-                });
-                // hide spinner 
-                document.getElementById('spinner').style.display = 'none';
-            }
-            else{
-                // clear previous data
-                displayPhones.innerHTML = '';
-
-                // display no data
-                document.getElementById('display-total').innerHTML = `
-                <div class="alert alert-danger" role="alert">
-                    No Data Found!
-                </div>
-                `
-
-                // hide spinner 
-                document.getElementById('spinner').style.display = 'none';
-            }
-        })
-    } 
-    // api error
-    catch (error) {
-        console.log('API Error!');
+                            `
+                        displayPhones.appendChild(div);
+                    });
+                    // hide spinner 
+                    document.getElementById('spinner').style.display = 'none';
+                }
+                else{
+                    // clear previous data
+                    displayPhones.innerHTML = '';
+    
+                    // display no data
+                    document.getElementById('display-total').innerHTML = `
+                    <div class="alert alert-danger" role="alert">
+                        No Data Found!
+                    </div>
+                    `
+    
+                    // hide spinner 
+                    document.getElementById('spinner').style.display = 'none';
+                }
+            })
+        } 
+        // api error
+        catch (error) {
+            console.log('API Error!');
+        }
     }
 }
 
-// display details
-const displayDetails = id => {
-    try {
-        // display spinner 
-        document.getElementById('spinner').style.display = 'block';
-
-        // clear previous data
-        displayPhones.innerHTML = '';
-
-        // get display field id
-        const displayPhones = document.getElementById('display-phones');
-
-        const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-
-        console.log(url);
-
-    } catch (error) {
-        console.log('API Error!');
-    }
-}
 
 // while pressing enter
 var input = document.getElementById('search-input');
