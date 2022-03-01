33a52308073d2b2f167f1a596a29b97e1112193d
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-01 12:59:28 +0600

    comment & display phone function

--- js/app.js
+++ js/app.js
@@ -15,80 +15,89 @@ const phoneSearch = _ => {
         `
     }
     else{
-        try {
-            const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    
-            fetch(url)
-            .then(response => response.json())
-            .then(phones => {
-                // get display field id
-                const displayPhones = document.getElementById('display-phones');
-                
-                if(phones.status == true){
-                    // clear previous result
-                    clearPreviousResult('display-phones');
-                    clearPreviousResult('phone-details');
-    
-                    // display total result
-                    document.getElementById('display-total').innerHTML = `
-                    <div class="alert alert-success" role="alert">
-                        ${phones.data.length} Phones Found
-                    </div>`;
-    
-                    phones.data.forEach(phone => {
-                        // create a div
-                        const div = document.createElement('div');
-    
-                        // display each result
-                        div.innerHTML = `
-                            <div  class="col">
-                                <div class="card h-100">
-                                <img src="${phone.image}" class="image-fluid card-img-top p-3" alt="${phone.phone_name} image" />
-                                <div class="card-body">
-                                    <h3 class="card-title">${phone.phone_name}</h3>
-                                    <h5 class="card-title text-muted">${phone.brand}</h5>
-                                    <p class="card-text">
-                                    Phone This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit
-                                    longer.
-                                    </p>
-                                </div>
-                                <div class="card-footer">
-                                    <div 
-                                    class="text-primary"
-                                    onclick="displayPhoneDetails('${phone.slug}')"
-                                    >
-                                    See Details</div>
-                                </div>
-                                </div>
-                            </div>
-                            `
-                        displayPhones.appendChild(div);
-                    });
-                    // hide spinner 
-                    displaySpinner(false);
-                }
-                else{
-                    // clear previous data
-                    displayPhones.innerHTML = '';
-                    clearPreviousResult('display-phones');
-    
-                    // display no data
-                    document.getElementById('display-total').innerHTML = `
-                    <div class="alert alert-danger" role="alert">
-                        No Data Found!
-                    </div>
-                    `
+        displayPhone(searchText);
+    }
+}
 
-                    // hide spinner 
-                    displaySpinner(false);
-                }
-            })
-        } 
-        // api error
-        catch (error) {
-            console.log('API Error!');
-        }
+const displayPhone = searchText => {
+    try {
+        // get dynamic search
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+
+        fetch(url)
+        .then(response => response.json())
+        .then(phones => {
+            // get display field id
+            const displayPhones = document.getElementById('display-phones');
+            
+            // if search result found
+            if(phones.status == true){
+                // clear previous result
+                clearPreviousResult('display-phones');
+                clearPreviousResult('phone-details');
+
+                // display total result
+                document.getElementById('display-total').innerHTML = `
+                <div class="alert alert-success" role="alert">
+                    ${phones.data.length} Phones Found
+                </div>`;
+
+                // seperate each phone
+                phones.data.forEach(phone => {
+                    // create a div
+                    const div = document.createElement('div');
+
+                    // display each result
+                    div.innerHTML = `
+                        <div  class="col">
+                            <div class="card h-100">
+                            <img src="${phone.image}" class="image-fluid card-img-top p-3" alt="${phone.phone_name} image" />
+                            <div class="card-body">
+                                <h3 class="card-title">${phone.phone_name}</h3>
+                                <h5 class="card-title text-muted">${phone.brand}</h5>
+                                <p class="card-text">
+                                Phone This is a wider card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit
+                                longer.
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <div 
+                                class="text-primary"
+                                onclick="displayPhoneDetails('${phone.slug}')"
+                                >
+                                See Details</div>
+                            </div>
+                            </div>
+                        </div>
+                        `
+                    // append div to display
+                    displayPhones.appendChild(div);
+                });
+                // hide spinner 
+                displaySpinner(false);
+            }
+            // if no search result found
+            else{
+                // clear previous data
+                displayPhones.innerHTML = '';
+                clearPreviousResult('display-phones');
+
+                // display no data
+                document.getElementById('display-total').innerHTML = `
+                <div class="alert alert-danger" role="alert">
+                    No Data Found!
+                </div>
+                `
+
+                // hide spinner 
+                displaySpinner(false);
+            }
+        })
+    } 
+    // api error
+    catch (error) {
+        console.log('API Search Error!');
     }
 }
 
@@ -98,6 +107,7 @@ const displayPhoneDetails = id => {
     displaySpinner(true);
 
     try {
+        // get dynamic url
         const url = `https://openapi.programming-hero.com/api/phone/${id}`
 
         fetch(url)
@@ -158,7 +168,7 @@ const displayPhoneDetails = id => {
     } 
     // api error
     catch (error) {
-        console.log('API Error!');
+        console.log('API Search ID Error!');
     }
 }
 
